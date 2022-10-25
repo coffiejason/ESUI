7815bcd2d56f20d6d481a208b3cd976f60bb7463
coffiejason <coffiejasoncj@gmail.com>
2022-10-25 04:34:15 +0000

    readme update

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, {useRef,useEffect,useState} from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 
 import axios from 'axios'
 
@@ -26,10 +26,10 @@ const api = axios.create({
   baseURL: `https://camiestas-futbol.effectstudios.co/api/v1/banner`
 })
 
-const handleClick = (e:any) =>{
+const handleClick = (e: any) => {
 
-    
-  e.target.className === 'inactive' ?  e.target.className = 'active' : e.target.className = 'inactive'
+
+  e.target.className === 'inactive' ? e.target.className = 'active' : e.target.className = 'inactive'
 
   activeId[0] = e.target.id
 
@@ -37,46 +37,57 @@ const handleClick = (e:any) =>{
 
 }
 
-let banners: any[] = [{
-  id: '00000',
-  content: 'Your favourite customized',
-  image: '',
-  btn_text: 'Shop now',
-  link: ''
-}]
+let i = 0
 
 const carousel = () => {
+  const [bannersState, setBannersState] = useState(() => {
+    return [{ id: '00000', content: 'Your favourite customize Club Jerseys.', image: '', btn_text: 'Shop now', link: '' }]
+  });
 
-  useEffect(()=>{
-    api.get('/').then(res =>{
-      res.data.banners.data.map((item:any)=>{
-        banners.push(
-          {
-            id: item.id,
-            content: item.content,
-            image: item.image,
-            btn_text: item.btn_text,
-            link: item.link
-          }
-        )
-      })
+  const [active, setActive] = useState(() => { return { id: '00000', content: 'Your favourite customize', image: '', btn_text: 'Shop now', link: '' } })
+
+  const changeActive = (banners: any[]) => {
+
+    let len = bannersState.length -1
+
+    
+    setActive(bannersState[i])
+    len > i ? i++ : i = 0
+    console.log(i)
+  }
+
+
+  useEffect(() => {
+    api.get('/').then(res => {
+      setBannersState(res.data.banners.data);
+      console.log(bannersState);
     })
-  },[])
-  
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      changeActive(bannersState)
+    }, 5 * 1000);
+
+    return () => clearInterval(interval);
+
+  }, [bannersState])
+
 
   return (
     <div className='carousel'>
-      <h2>{banners[0].content} <span>Club Jerseys.</span></h2>
+      <h2>{String(active.content).replace('<p>','').replace('</p>','').replace('null','')} {/*<span>Club Jerseys.</span>*/}</h2>
+      {/* {active.content} */}
       <div className='hero-buttons'>
         <div className='nav-buttons'>
-          <div className={'inactive'} /*ref={useRef1}*/ id={'1'} onClick={(e)=>handleClick(e)}></div>
-          <div className={'inactive'} /*ref={useRef2}*/ id={'2'} onClick={(e)=>handleClick(e)}></div>
-          <div className={'inactive'} /*ref={useRef3}*/ id={'3'} onClick={(e)=>handleClick(e)}></div>
-          <div className={'inactive'} /*ref={useRef4}*/ id={'4'} onClick={(e)=>handleClick(e)}></div>
-          <div className={'inactive'} /*ref={useRef5}*/ id={'5'} onClick={(e)=>handleClick(e)}></div>
+          <div className={'inactive'} /*ref={useRef1}*/ id={'1'} onClick={(e) => handleClick(e)}></div>
+          <div className={'inactive'} /*ref={useRef2}*/ id={'2'} onClick={(e) => handleClick(e)}></div>
+          <div className={'inactive'} /*ref={useRef3}*/ id={'3'} onClick={(e) => handleClick(e)}></div>
+          <div className={'inactive'} /*ref={useRef4}*/ id={'4'} onClick={(e) => handleClick(e)}></div>
+          <div className={'inactive'} /*ref={useRef5}*/ id={'5'} onClick={(e) => handleClick(e)}></div>
         </div>
         <div className='shop-now-button'>
-          <p>{banners[0].btn_text}</p>
+          <p>{active.btn_text}</p>
           <div className='shop-now-icon'>
             <img src={bag} />
           </div>
